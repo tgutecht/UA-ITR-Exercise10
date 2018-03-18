7ab4f67e1d7fd816d7ab45947ffec40e037f032b
tgutecht <35664243+tgutecht@users.noreply.github.com>
2018-03-18 17:14:02 -0500

    part 7 completed

--- UA-ITR-Exercise10/script.js
+++ UA-ITR-Exercise10/script.js
@@ -45,8 +45,9 @@ if (age >18) {
 }
 
 //demo7 produce a random number
-var part6Number = Math.floor((Math.random() * 100) + 1);
-document.getElementById("part6Number").innerHTML = "exercise part6:" + " " + part6Number;
+var randy = Math.floor((Math.random() * 100) + 1);
+var part6Number = randy;
+document.getElementById("part6Number").innerHTML = "exercise part-6:" + " " + part6Number;
 var part6Result ;
 if (part6Number > 50) {
     part6Result = "Above 50.";
@@ -54,3 +55,14 @@ if (part6Number > 50) {
     part6Result = "Below or equal to 50.";
 }
 document.getElementById("part6Result").innerHTML = part6Result;
+
+// demo8
+var part7Number = randy;
+document.getElementById("part7Number").innerHTML = "exercise part-7:" + " " + part7Number;
+var part7Result;
+if (part7Number<25) { part7Result = "Below 25";}
+else if (part7Number>=25 && part7Number<50){ part7Result = "Between 25 and 50.";}
+else if (part7Number>=50 && part7Number<75) {part7Result = "Between 50 and 75.";}
+else {part7Result="Above 75.";}
+
+document.getElementById("part7Result").innerHTML = part7Result
